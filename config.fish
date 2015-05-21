482c24f6b6b3196dbe1579f60088f3e728d63fe8
Markus Pilman <mpilman@inf.ethz.ch>
2015-05-21 14:50:18 +0200

    made it work on docker

--- config.fish
+++ config.fish
@@ -1,3 +1,12 @@
 set -x DOCKER_CERT_PATH /Users/mpilman/.boot2docker/certs/boot2docker-vm
 set -x DOCKER_HOST tcp://192.168.59.103:2376
 set -x DOCKER_TLS_VERIFY 1
+set -x LANG "en_US.UTF-8"
+set -x LC_COLLATE "en_US.UTF-8"
+set -x LC_CTYPE "UTF-8"
+set -x LC_MESSAGES "en_US.UTF-8"
+set -x LC_MONETARY "en_US.UTF-8"
+set -x LC_NUMERIC "en_US.UTF-8"
+set -x LC_TIME "en_US.UTF-8"
+set -x TERM xterm-256color
+
